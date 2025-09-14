e230a8c15a0837c050865e072980b09b6d4e1a70
Zia Ahmed <ziaahmedf17@gmail.com>
2025-09-14 20:59:32 +0500

    Initial Commit

---
+++ JordansIOSSwift/JordansIOSSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  JordansIOSSwiftApp.swift
+//  JordansIOSSwift
+//
+//  Created by Zi on 14/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct JordansIOSSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JordansIOSSwiftTests/JordansIOSSwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  JordansIOSSwiftTests.swift
+//  JordansIOSSwiftTests
+//
+//  Created by Zi on 14/09/2025.
+//
+
+import Testing
+@testable import JordansIOSSwift
+
+struct JordansIOSSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
