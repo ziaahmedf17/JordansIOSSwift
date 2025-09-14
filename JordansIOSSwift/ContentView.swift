9ab59a13261c86b38f45ed52dedff109914971a6
Zia Ahmed <ziaahmedf17@gmail.com>
2025-09-14 21:40:03 +0500

    Navigation bar added

--- JordansIOSSwift/ContentView.swift
+++ JordansIOSSwift/ContentView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-    
+        TabView{
+            NavigationStack{
+                ScrollView{
+                    VStack{
+                        Text("Top Brands")
+                    }.ignoresSafeArea().navigationBarTitle("Jordans")
+                        .navigationBarItems(trailing: Image(systemName: "bell.fill")
+                            .resizable().scaledToFit().frame(width: 30, height: 30).opacity(0.3))
+                }
+            }
+        }
     }
 }
 
